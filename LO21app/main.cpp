94a47116f09985cc2a6365eacd292d2c4571580f
Loic1204 <loicderyckere@hotmail.fr>
2015-06-04 16:39:32 +0200

    design pattern singleton classe Tache abstraite fonctions Tache afficher operator<< Tache

--- LO21app/main.cpp
+++ LO21app/main.cpp
@@ -7,14 +7,14 @@ int main(int argc, char * argv[]) {
 
 //    Projet proj("1", "Projet machin", "truc.xml", Date(9,6,2015));
 //    Unitaire t1 = proj.ajouterUnitaire("t1", "Tache unitaire de test", Date(10,6,2015), Date(20,6,2015), Duree(1,20), true);
-    Unitaire t1 = Unitaire (Date(10,6,2015), Date(20,6,2015), "t1", "Tache unitaire de test", Duree(1,20), true);
-    Composite t2 = Composite("t2", "Tache composite de test", Date(15,6,2015), Date(25,6,2015));
+//    Unitaire t1 = Unitaire (Date(10,6,2015), Date(20,6,2015), "t1", "Tache unitaire de test", Duree(1,20), true);
+//    Composite t2 = Composite("t2", "Tache composite de test", Date(15,6,2015), Date(25,6,2015));
 
     MainWindow wind;
     wind.show();
 
 //    wind.showUnitaire(t1);
-    wind.showComposite(t2);
+//    wind.showComposite(t2);
 
 
 

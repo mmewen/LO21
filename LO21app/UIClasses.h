c81ca26c89b2c0f408df3c7d55c14ca20c27f781
Mewen <mewenmichel@yahoo.fr>
2015-06-04 17:11:57 +0200

    Mini commit pour merge

---
+++ LO21app/UIClasses.h
@@ -0,0 +1,13 @@
+#ifndef UICLASSES_H
+#define UICLASSES_H
+
+//class TreeViewModel {
+//    QStandardItemModel
+//    // le modèle
+//    // le tableau qui lie les items aux tâches
+
+//};
+
+// insert : beginInsertRows() and endInsertRows()
+
+#endif // UICLASSES_H
--- LO21app/main.cpp
+++ LO21app/main.cpp
@@ -7,8 +7,15 @@ int main(int argc, char * argv[]) {
 
 //    Projet proj("1", "Projet machin", "truc.xml", Date(9,6,2015));
 //    Unitaire t1 = proj.ajouterUnitaire("t1", "Tache unitaire de test", Date(10,6,2015), Date(20,6,2015), Duree(1,20), true);
-    Unitaire t1 = Unitaire (Date(10,6,2015), Date(20,6,2015), "t1", "Tache unitaire de test", Duree(1,20), true);
-    Composite t2 = Composite("t2", "Tache composite de test", Date(15,6,2015), Date(25,6,2015));
+
+//    Unitaire t1 = Unitaire (Date(10,6,2015), Date(20,6,2015), "t1", "Tache unitaire de test", Duree(1,20), true);
+//    Composite t2 = Composite("t2", "Tache composite de test", Date(15,6,2015), Date(25,6,2015));
+
+    ProjetManager pjm = ProjetManager::getInstance();
+    Projet* p1 = pjm.ajouterProjet("p1", "Projet 1", "pj1.xml", Date(4,6,2015));
+    Composite* t1 = p1->ajouterComposite("t1", "Tache unitaire de test", Date(10,6,2015), Date(20,6,2015), Duree(1,20), true);
+    Composite* t2 = p1->ajouterComposite("t2", "Tache composite de test", Date(15,6,2015), Date(25,6,2015));
+
 
     MainWindow wind;
     wind.show();
